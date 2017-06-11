92aff114b71e8b8b610819dcc88d9e9e313c17e2
snake <adsloader99@gmail.com>
2017-06-11 16:15:21 +0900

    6일차java예제포함

--- app/src/main/java/com/example/snake/androjavaexample/activity_1_2.java
+++ app/src/main/java/com/example/snake/androjavaexample/activity_1_2.java
@@ -1,13 +1,28 @@
 package com.example.snake.androjavaexample;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_1_2 extends AppCompatActivity {
+
+    Button btnOk = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1_2);
+
+        btnOk = (Button)findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnOk.setText("Click~~!!");
+                btnOk.setTextColor(Color.parseColor("#333333"));
+                btnOk.setBackgroundColor(Color.parseColor("#FFFF33"));
+            }
+        });
     }
 }
