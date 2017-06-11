72f2b2e7dd169b674ad7ff4fea8c9190e2408c9d
snake <adsloader99@gmail.com>
2017-06-11 16:04:04 +0900

    6일차java예제포함

---
+++ app/src/main/java/com/example/snake/androjavaexample/activity_1_2.java
@@ -0,0 +1,13 @@
+package com.example.snake.androjavaexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_1_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1_2);
+    }
+}
